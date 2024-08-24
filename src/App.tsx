85a5056bd3cb53198eefbb17d5152acdceb8254a
Sahil <sahilahmed466@gmail.com>
2024-08-24 21:40:45 +0530

    feat: Setting up project

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import './App.css'
+
+function App() {
+  return (
+    <>
+      <div className='gridLinesBg h-screen w-screen'>
+        hello world
+      </div>
+    </>
+  )
+}
+
+export default App
---
+++ src/main.tsx
@@ -0,0 +1,12 @@
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import App from './App.tsx'
+import './index.css'
+import { ThemeProvider } from "@/components/theme-provider"
+
+createRoot(document.getElementById('root')!).render(
+  <StrictMode>
+    <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme"></ThemeProvider>
+    <App />
+  </StrictMode>,
+)
