f8b55e887915a966c37bc198f4ab4225b5ec086c
Sahil <sahilahmed466@gmail.com>
2024-08-27 15:55:07 +0530

    fix: Fixed Linkedin broken link

--- src/components/footer/SocialIcons.tsx
+++ src/components/footer/SocialIcons.tsx
@@ -4,7 +4,7 @@ import { MdAttachEmail } from "react-icons/md";
 function SocialIcons() {
   return (
     <div className="flex items-center gap-2 md:gap-4">
-      <a href="www.linkedin.com/in/sahil-ahmed-86222718a" target="_blank" className="cursor-pointer p-1 rounded-lg flex items-center justify-center">
+      <a href="https://www.linkedin.com/in/sahil-ahmed-86222718a" target="_blank" className="cursor-pointer p-1 rounded-lg flex items-center justify-center">
         <FaLinkedin className="text-[#5FFb17] hover:text-white w-6 h-6" />
       </a>
       <a href="https://github.com/1999-sahil" target="_blank" className="cursor-pointer p-1 rounded-lg flex items-center justify-center">
